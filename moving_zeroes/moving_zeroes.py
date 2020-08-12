a6270b795383d2771ad4d6a5cc071fe5ec74076a
cauld19 <50849710+cauld19@users.noreply.github.com>
2020-08-11 20:52:11 -0700

    non zero

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -3,9 +3,33 @@ Input: a List of integers
 Returns: a List of integers
 '''
 def moving_zeroes(arr):
-    # Your code here
+    ## create new array to reorder ele to
+    ## if ele in array is not 0 append to new arr
+    ## if ele is 0 insert at end of newarr
+    
+    ## or
+    
+    ## create array of non zero numbers
+    ## create array of zeros
+    ## extend array of zeros to end of non zeros array
+    newarr = []
+    newarr2 = []
 
-    pass
+    for i in range(0, len(arr)):
+        if arr[i] != 0:
+            newarr.append(arr[i])
+        else:
+            newarr2.append(arr[i])
+            
+    newarr.extend(newarr2)
+
+    # for j in range(0, len(arr)):
+    #     if arr[j] == 0:
+    #         newarr.insert(len(arr), arr[j])
+
+    
+
+    return newarr
 
 
 if __name__ == '__main__':
